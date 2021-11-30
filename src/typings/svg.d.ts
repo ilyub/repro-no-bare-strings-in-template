b232b1610d0f58041ab491b8b29959e7f0765c7d
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-11-30 13:44:15 +0300

    refactor(auto-eslint)

--- src/typings/svg.d.ts
+++ src/typings/svg.d.ts
@@ -1,5 +1,6 @@
 declare module "*.svg" {
   const svg: string;
+
   // eslint-disable-next-line import/no-default-export
   export default svg;
 }
--- src/typings/vue.d.ts
+++ src/typings/vue.d.ts
@@ -1,7 +1,9 @@
 declare module "*.vue" {
   import type { DefineComponent } from "vue";
+
   // eslint-disable-next-line @skylib/no-mutable-signature, @skylib/prefer-readonly
   const component: DefineComponent<object, object, unknown>;
+
   // eslint-disable-next-line import/no-default-export
   export default component;
 }
