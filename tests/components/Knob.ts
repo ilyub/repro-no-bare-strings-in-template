85d84d96bca63d2eb3a76dda18b9787ddc9bb58a
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-02 13:47:40 +0300

    test(fix,Knob): Fix for failed test

--- tests/components/Knob.ts
+++ tests/components/Knob.ts
@@ -3,7 +3,11 @@ import * as testUtils from "@vue/test-utils";
 import Knob from "@/components/Knob.vue";
 
 it("Knob", () => {
-  const wrapper = testUtils.mount(Knob);
+  const wrapper = testUtils.mount(Knob, {
+    props: {
+      "model-value": 1000
+    }
+  });
 
   expect(wrapper).toBeDefined();
 });
