39bc93b2a2b09fd2d75ac6dc72e56b736ac6041a
Ilya Lyubinskiy <admin.phpdev@gmail.com>
2021-12-07 00:34:23 +0300

    chore(eslint): Added "extraChoreLocations" option

--- .eslintrc.options.js
+++ .eslintrc.options.js
@@ -1,4 +1,11 @@
 module.exports = {
   es: true,
+  extraChoreLocations: [
+    "src/App.vue",
+    "src/boot/**",
+    "src/router/**",
+    "src/routes.ts",
+    "src/typings/**"
+  ],
   readonlyIgnoreTypes: ["App", "InjectionKey", "SetupContext"]
 };
